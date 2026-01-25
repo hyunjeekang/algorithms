de2493252e13b6edd74826d6ae3d7c208adfde04
hyunjeekang <hihihyunjj@gmail.com>
2026-01-25 12:16:35 +0900

    feat: add new solution for #2738(행렬 덧셈)

--- baekjoon/2738.py
+++ baekjoon/2738.py
@@ -1,7 +1,9 @@
 N, M = map(int, input().split())
 grid1 = [list(map(int, input().split())) for _ in range(N)]
 grid2 = [list(map(int, input().split())) for _ in range(N)]
-for r in range(N):
-    for c in range(M):
-        grid1[r][c] += grid2[r][c]
-for row in grid1: print(*row)
+# for r in range(N):
+#     for c in range(M):
+#         grid1[r][c] += grid2[r][c]
+# for row in grid1: print(*row)
+result = [[c+d for c, d in zip(a, b)] for a, b in zip(grid1, grid2)]
+for row in result: print(*row)
