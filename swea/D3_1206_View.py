2af0573d662849e2a3ac053fb1a1a6ac4b62064a
hyunjeekang <hihihyunjj@gmail.com>
2026-02-02 21:06:04 +0900

    refactor: improve #1206

--- swea/D3_1206_View.py
+++ swea/D3_1206_View.py
@@ -1,26 +1,18 @@
 def solve():
-    n = int(input())
+    line = input().split()
+    n = int(line[0])
     buildings = list(map(int, input().split()))
-    check = [1, -1, 2, -2]
+
     ans = 0
-    for j in range(2, n-2):
-        cur_building = buildings[j]
-        sub = 255
-        view = True
-        for k in range(4):
-            check_idx = j + check[k]
-            if 0 <= check_idx < n:
-                check_building = buildings[check_idx]
-                if view:
-                    if cur_building > check_building:
-                        sub = min(sub, cur_building - check_building)
-                    else:
-                        sub = 0
-                        view = False
-        ans += sub
+    for i in range(2, n - 2):
+        max_neighbor = max(buildings[i-2], buildings[i-1], buildings[i+1], buildings[i+2])
+        
+        if buildings[i] > max_neighbor:
+            ans += buildings[i] - max_neighbor
 
     return ans
 
 for i in range(1, 11):
     result = solve()
-    print(f"#{i} {result}")
+    if result is not None:
+        print(f"#{i} {result}")
