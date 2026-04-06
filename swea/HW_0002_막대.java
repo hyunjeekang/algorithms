ea620bf0fe00753892f0d2649d33e795fcf37ced
hyunjeekang <hihihyunjj@gmail.com>
2026-04-06 22:48:49 +0900

    feat: solved homework_250406 막대

---
+++ swea/HW_0002_막대.java
@@ -0,0 +1,15 @@
+class HW_0002_막대{
+
+    public static void main(String[] args) {
+        int[] memo = new int[10];
+        memo[1] = 2; //파, 노
+        memo[2] = 5; //파파, 노파, 노노, 노파, 빨
+
+        for(int i = 3; i < 7; i++){
+            memo[i] = memo[i-1]*2 + memo[i-2];
+        }
+
+        System.out.println(memo[6]);
+    }
+    
+}
