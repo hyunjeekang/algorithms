2d2f708f6a42ca36ac905eae32b71721515e1024
hyunjeekang <hihihyunjj@gmail.com>
2026-04-07 19:48:06 +0900

    refactor: add top-down method

--- baekjoon/BOJ1149.java
+++ baekjoon/BOJ1149.java
@@ -22,22 +22,44 @@ public class BOJ1149 {
             houses[n][2] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(up(N));
+        // System.out.println(up(N));
+        System.out.println(Math.min(down(N, 0), Math.min(down(N, 1), down(N, 2))));
     }
 
-    private static int up(int N){
+    private static int up(int n){
         // n == 1
         for(int i = 0; i < 3; i++){
             dp[1][i] = houses[1][i];
         }
 
-        for(int h = 2; h < N+1; h++){
+        for(int h = 2; h < n+1; h++){
             dp[h][0] = Math.min(dp[h-1][1], dp[h-1][2]) + houses[h][0];
             dp[h][1] = Math.min(dp[h-1][0], dp[h-1][2]) + houses[h][1];
             dp[h][2] = Math.min(dp[h-1][0], dp[h-1][1]) + houses[h][2];
         }
 
-        return Math.min(dp[N][0], Math.min(dp[N][1], dp[N][2]));
+        return Math.min(dp[n][0], Math.min(dp[n][1], dp[n][2]));
+    }
+
+    private static int down(int n, int color){
+
+        // 기저 조건
+        if(n == 1){
+            return dp[1][color] = houses[1][color];
+        }
+
+        // 이미 계산된 경우 
+        if(dp[n][color] != 0){
+            return dp[n][color];
+        }
+
+        // 계산 안 된 경우
+        if(color == 0) dp[n][0] = Math.min(down(n-1, 1), down(n-1, 2)) + houses[n][0];
+        else if(color == 1) dp[n][1] = Math.min(down(n-1, 0), down(n-1, 2)) + houses[n][1];
+        else dp[n][2] = Math.min(down(n-1, 0), down(n-1, 1)) + houses[n][2];
+        
+        // 계산 완료 값 반환
+        return dp[n][color];
     }
     
 }
